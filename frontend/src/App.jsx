0dca4c42445f4769f142d2d08e7724bb714d12ab
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-06-13 15:07:04 +0530

    useNavigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
 
   return (
     <>
       <div>
         <h1>Welcome to PSR</h1>
-        <button className="users-btn">Users</button>
+        <button className="users-btn" onClick={() => {navigate('/users')}}>Users</button>
       </div>
     </>
   )
